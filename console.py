8b6e48264cf64d14c6ce0068b7448f0ab55f277f
klupzor <lopezgand@gmail.com>
2019-07-01 23:11:59 -0500

    Creating create function

--- console.py
+++ console.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     '''Hbnbc command'''
-    intro = 'Welcome to the Hbnb shell.   Type help or ? to list commands.\n'
+    intro = 'Welcome to the Hbnb shell. Type help or ? to list commands.\n'
     prompt = '(hbnb) '
     file = None
 
@@ -14,10 +15,25 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         return True
 
+    def do_create(self, line):
+        if len(line) == 0:
+            print("** class name missing **")
+        elif line != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            my_model = BaseModel()
+            my_model.save()
+            print(my_model.id)
+
+    def help_create(self):
+        print("Creates a new instance of BaseModel, saves "
+              "it (to the JSON file) and prints the id.\n"
+              "Ex: $ create BaseModel\n")
+
     def help_quit(self):
         print("Quit command to exit the program \n")
 
-    def help_(self):
+    def help_EOF(self):
         print("Quit command to exit the program \n")
 
     def emptyline(self):
