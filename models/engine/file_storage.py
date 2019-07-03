f482813e9c3a9a90edcce7d6b1deec62dbbfe671
Jose Medina <fermed28@gmail.com>
2019-07-03 02:56:51 +0000

    [fixes] import models to filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,12 @@
 
 import json
 from ..base_model import BaseModel
-from ..user import User
+from ..state import State
+from ..city import City
+from ..amenity import Amenity
+from ..place import Place
+from ..review import Review
+
 
 class FileStorage():
     '''
