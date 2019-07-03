b70f99068c16d36b4b982873d72b27723ee21e3d
klupzor <lopezgand@gmail.com>
2019-07-03 16:28:14 -0500

    Writing comments and add doc case

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Test for User"""
 import unittest
+import models
 from models.base_model import BaseModel
 
 
-class TestUser(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
     '''Test BaseModel'''
     def setUp(self):
         self.my_model = BaseModel()
@@ -12,23 +13,39 @@ class TestUser(unittest.TestCase):
         self.my_model.my_number = 89
         self.my_model_json = self.my_model.to_dict()
 
+    def test_doc_module(self):
+        '''validate documentation module'''
+        document = models.base_model.__doc__
+        self.assertIsNotNone(document)
+        
+    def test_doc_module(self):
+        '''validate documentation class'''
+        document = BaseModel.__doc__
+        self.assertIsNotNone(document)
+
     def test_has_attribute_id(self):
+        '''test attribute'''
         self.assertTrue(hasattr(self.my_model, "id"))
 
     def test_has_attribute_created_at(self):
+        '''test attribute'''
         self.assertTrue(hasattr(self.my_model, "created_at"))
 
     def test_has_attribute_updated_at(self):
+        '''test attribute'''
         self.assertTrue(hasattr(self.my_model, "updated_at"))
 
     def test_has_attribute_class(self):
+        '''test attribute'''
         self.assertTrue(hasattr(self.my_model_json, "__class__"))
 
     def test_values(self):
+        '''test values'''
         self.assertAlmostEqual(self.my_model.name, "Holberton")
         self.assertAlmostEqual(self.my_model.my_number, 89)
 
     def test_exist(self):
+        '''test attribute is not none'''
         self.assertIsNotNone(str(self.my_model))
         self.assertIsNotNone(self.my_model.to_dict())
 
