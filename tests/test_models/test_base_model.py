aa437f284d6a881acf1c3a13a63eff0b2a2f2048
klupzor <lopezgand@gmail.com>
2019-07-03 15:38:06 -0500

    Create BaseModel Test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,8 +3,27 @@
 import unittest
 from models.base_model import BaseModel
 
-class TestUser(unittest.TestCase):
-    
 
-if __name__ = '__main__':
+class TestUser(unittest.TestCase):
+    '''Test BaseModel'''
+    def setUp(self):
+        self.my_model = BaseModel()
+        self.my_model.name = "Holberton"
+        self.my_model.my_number = 89
+        self.my_model_json = self.my_model.to_dict()
+
+    def test_has_attribute(self):
+        self.assertTrue(hasattr(self.my_model, "id"))
+        self.assertTrue(hasattr(self.my_model, "created_at"))
+        self.assertTrue(hasattr(self.my_model, "updated_at"))
+        self.assertTrue(hasattr(self.my_model_json, "__class__"))
+
+    def test_values(self):
+        self.assertAlmostEqual(self.my_model.name, "Holberton")
+        self.assertAlmostEqual(self.my_model.my_number, 89)
+        self.assertIsNotNone(str(self.my_model))
+        self.assertIsNotNone(self.my_model.to_dict())
+
+
+if __name__ == '__main__':
     unittest.main()
