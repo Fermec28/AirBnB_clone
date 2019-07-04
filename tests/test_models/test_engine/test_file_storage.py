245eef45e790d755ed54294c83688377df680974
Jose Medina <fermed28@gmail.com>
2019-07-04 05:30:43 +0000

    [add] test file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Test for BaseModel"""
+import unittest
+import models
+
+
+class TestFileStorage(unittest.TestCase):
+    '''Test FileStorage'''
+    def setUp(self):
+        pass
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+if __name__ == '__main__':
+    unittest.main()
